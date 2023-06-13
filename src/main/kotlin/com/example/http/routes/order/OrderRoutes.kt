dc60ece9a949f52e8689d81ade0360cfb0f67801
박성현 <pjhomg5530@naver.com>
2023-06-13 21:03:58 +0900

    :sparkles: 특정 주문 합산 반환

--- src/main/kotlin/com/example/http/routes/order/OrderRoutes.kt
+++ src/main/kotlin/com/example/http/routes/order/OrderRoutes.kt
@@ -19,3 +19,12 @@ fun Route.getOrderRoute() {
         call.respond(order)
     }
 }
+
+fun Route.totalizeOrderRoute() {
+    get("/order/{id?}/total") {
+        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+        val order = orderStorage.find { it.number == id } ?: return@get call.respondText("Not Found", status = HttpStatusCode.NotFound)
+        val total = order.contents.sumOf { it.amount * it.price }
+        call.respond(total)
+    }
+}

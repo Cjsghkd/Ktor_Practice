9ea817e488958ada697dc3df6cdcf31a3bf18518
박성현 <pjhomg5530@naver.com>
2023-06-13 21:04:44 +0900

    :sparkles: Routing에 경로 등록

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,6 +1,9 @@
 package com.example.plugins
 
 import com.example.http.routes.customer.customerRouting
+import com.example.http.routes.order.getOrderRoute
+import com.example.http.routes.order.listOrdersRoute
+import com.example.http.routes.order.totalizeOrderRoute
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
@@ -8,5 +11,8 @@ import io.ktor.server.application.*
 fun Application.configureRouting() {
     routing {
         customerRouting()
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
     }
 }

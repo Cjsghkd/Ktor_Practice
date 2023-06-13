b72d1c5d05d7e827d70b9d73f3bb2995b632a817
박성현 <pjhomg5530@naver.com>
2023-06-13 20:26:45 +0900

    :sparkles: 모든 고객 반환

--- src/main/kotlin/com/example/http/routes/customer/CustomerRoutes.kt
+++ src/main/kotlin/com/example/http/routes/customer/CustomerRoutes.kt
@@ -1,11 +1,16 @@
 package com.example.http.routes.customer
 
+import com.example.http.models.customer.customerStorage
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.customerRouting() {
     route("/customer") {
         get {
-
+            if (customerStorage.isNotEmpty()) call.respond(customerStorage)
+            else call.respondText("No customers found", status = HttpStatusCode.OK)
         }
         get("{id?}") {
 

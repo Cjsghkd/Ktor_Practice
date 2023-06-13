1234ba1943aa2c812f4cc121ea3a2adc04a8ce5a
박성현 <pjhomg5530@naver.com>
2023-06-13 20:20:40 +0900

    :sparkles: CustomerRoutes 생성

---
+++ src/main/kotlin/com/example/http/routes/customer/CustomerRoutes.kt
@@ -0,0 +1,20 @@
+package com.example.http.routes.customer
+
+import io.ktor.server.routing.*
+
+fun Route.customerRouting() {
+    route("/customer") {
+        get {
+
+        }
+        get("{id?}") {
+
+        }
+        post {
+
+        }
+        delete {
+
+        }
+    }
+}

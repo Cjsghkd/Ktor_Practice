2375b8db1157f050f6d6ea198151ba02a960da96
박성현 <pjhomg5530@naver.com>
2023-06-13 20:33:13 +0900

    :sparkles: 특정 고객 반환

--- src/main/kotlin/com/example/http/routes/customer/CustomerRoutes.kt
+++ src/main/kotlin/com/example/http/routes/customer/CustomerRoutes.kt
@@ -13,7 +13,9 @@ fun Route.customerRouting() {
             else call.respondText("No customers found", status = HttpStatusCode.OK)
         }
         get("{id?}") {
-
+            val id = call.parameters["id"] ?: return@get call.respondText("Missing id", status = HttpStatusCode.BadRequest)
+            val customer = customerStorage.find { it.id == id } ?: return@get call.respondText("No customer with id $id", status = HttpStatusCode.NotFound)
+            call.respond(customer)
         }
         post {
 

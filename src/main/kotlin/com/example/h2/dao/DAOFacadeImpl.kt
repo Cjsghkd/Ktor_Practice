ac69d0a35c0dd98cedfb7ece115b99feb641e3ab
박성현 <pjhomg5530@naver.com>
2023-06-14 10:08:35 +0900

    :sparkles: 새로운 기사 추가

--- src/main/kotlin/com/example/h2/dao/DAOFacadeImpl.kt
+++ src/main/kotlin/com/example/h2/dao/DAOFacadeImpl.kt
@@ -4,6 +4,7 @@ import com.example.h2.dao.DatabaseFactory.dbQuery
 import com.example.h2.models.Article
 import com.example.h2.models.Articles
 import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 
@@ -25,8 +26,12 @@ class DAOFacadeImpl: DAOFacade {
             .singleOrNull()
     }
 
-    override suspend fun addNewArticle(title: String, body: String): Article? {
-        TODO("Not yet implemented")
+    override suspend fun addNewArticle(title: String, body: String): Article? = dbQuery {
+        val insertStatement = Articles.insert {
+            it[Articles.title] = title
+            it[Articles.body] = body
+        }
+        insertStatement.resultedValues?.singleOrNull()?.let(::resultRowToArticle)
     }
 
     override suspend fun editArticle(id: Int, title: String, body: String): Boolean {

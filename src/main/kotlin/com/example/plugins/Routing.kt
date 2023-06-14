9c7ce623a28aa0546b70fba3764080b8e65ee13e
박성현 <pjhomg5530@naver.com>
2023-06-14 20:09:55 +0900

    :sparkles: articleRouting 추가

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,11 +1,11 @@
 package com.example.plugins
 
+import com.example.h2.routes.articleRouting
 import com.example.http.routes.customer.customerRouting
 import com.example.http.routes.order.getOrderRoute
 import com.example.http.routes.order.listOrdersRoute
 import com.example.http.routes.order.totalizeOrderRoute
 import io.ktor.server.routing.*
-import io.ktor.server.response.*
 import io.ktor.server.application.*
 
 fun Application.configureRouting() {
@@ -14,5 +14,6 @@ fun Application.configureRouting() {
         listOrdersRoute()
         getOrderRoute()
         totalizeOrderRoute()
+        articleRouting()
     }
 }

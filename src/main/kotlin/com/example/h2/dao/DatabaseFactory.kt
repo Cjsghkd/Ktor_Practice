51cf1892b62fcd22436d08f304ef623097f80268
박성현 <pjhomg5530@naver.com>
2023-06-14 09:52:26 +0900

    :sparkles: 유틸리티 함수 생성

--- src/main/kotlin/com/example/h2/dao/DatabaseFactory.kt
+++ src/main/kotlin/com/example/h2/dao/DatabaseFactory.kt
@@ -1,8 +1,10 @@
 package com.example.h2.dao
 
 import com.example.h2.models.Articles
+import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseFactory {
@@ -14,4 +16,7 @@ object DatabaseFactory {
             SchemaUtils.create(Articles)
         }
     }
+
+    suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
 }

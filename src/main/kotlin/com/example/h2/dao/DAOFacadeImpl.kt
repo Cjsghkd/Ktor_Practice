8a87d18237efdef2bda13b548ecd22075c81b92b
박성현 <pjhomg5530@naver.com>
2023-06-14 10:03:04 +0900

    :sparkles: 모든 기사 가져오기

--- src/main/kotlin/com/example/h2/dao/DAOFacadeImpl.kt
+++ src/main/kotlin/com/example/h2/dao/DAOFacadeImpl.kt
@@ -1,10 +1,20 @@
 package com.example.h2.dao
 
+import com.example.h2.dao.DatabaseFactory.dbQuery
 import com.example.h2.models.Article
+import com.example.h2.models.Articles
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.selectAll
 
 class DAOFacadeImpl: DAOFacade {
-    override suspend fun allArticles(): List<Article> {
-        TODO("Not yet implemented")
+    private fun resultRowToArticle(row: ResultRow) = Article(
+        id = row[Articles.id],
+        title = row[Articles.title],
+        body = row[Articles.body]
+    )
+
+    override suspend fun allArticles(): List<Article> = dbQuery {
+        Articles.selectAll().map(::resultRowToArticle)
     }
 
     override suspend fun articles(id: Int): Article? {

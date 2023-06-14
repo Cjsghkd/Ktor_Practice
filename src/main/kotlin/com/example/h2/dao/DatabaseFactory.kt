52d6619c653968088c879de37cb71cf832d8671b
박성현 <pjhomg5530@naver.com>
2023-06-14 09:49:19 +0900

    :sparkles: DB Table 생성

--- src/main/kotlin/com/example/h2/dao/DatabaseFactory.kt
+++ src/main/kotlin/com/example/h2/dao/DatabaseFactory.kt
@@ -1,11 +1,17 @@
 package com.example.h2.dao
 
+import com.example.h2.models.Articles
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseFactory {
     fun init() {
         val driverClassName = "org.h2.Driver"
         val jdbcURL = "jdbc:h2:~/ktor_practice"
         val database = Database.connect(jdbcURL, driverClassName)
+        transaction(database) {
+            SchemaUtils.create(Articles)
+        }
     }
 }
